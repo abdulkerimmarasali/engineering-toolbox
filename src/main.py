a63008ed02b5a4c57b8f66c9c8857184884c681c
abdulkerimmarasali <abdulkerimmarasali@gmail.com>
2026-02-25 14:58:06 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
+# src/main.py
+import sys
+from PyQt5.QtWidgets import QApplication
 
+from .app_shell import AppShell
+
+
+def main() -> None:
+    app = QApplication(sys.argv)
+    win = AppShell()
+    win.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()

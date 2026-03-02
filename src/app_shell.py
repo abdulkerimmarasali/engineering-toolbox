3691ae51e9628b927c57a823d1334b437ef2f2ed
abdulkerimmarasali <abdulkerimmarasali@gmail.com>
2026-03-02 15:46:48 +0300

    Update app_shell.py

--- src/app_shell.py
+++ src/app_shell.py
@@ -12,8 +12,7 @@ from PyQt5.QtWidgets import (
     QListWidget, QListWidgetItem, QFrame
 )
 
-from routes import CATEGORIES
-
+from src.routes import CATEGORIES
 
 @dataclass(frozen=True)
 class ModuleSpec:

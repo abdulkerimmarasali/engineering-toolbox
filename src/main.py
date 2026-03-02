5946b4f367ff50f9caa16cf562aa434807cf0210
abdulkerimmarasali <abdulkerimmarasali@gmail.com>
2026-03-02 16:18:14 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
+import os
 import sys
-from PyQt5.QtWidgets import QApplication
 
-from app_shell import AppShell  # <-- DİKKAT: src.app_shell değil
+# EXE ve normal çalıştırma için: src klasörünü sys.path'e ekle
+BASE_DIR = os.path.dirname(sys.executable) if getattr(sys, "frozen", False) else os.path.dirname(__file__)
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
+
+from PyQt5.QtWidgets import QApplication
+from app_shell import AppShell  # artık bulunur
 
 def main():
     app = QApplication(sys.argv)
